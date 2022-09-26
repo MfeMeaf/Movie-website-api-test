9001ccd9d6bfe146e12597400a2d942821518e92
Felix Lamu <80445981+MfeMeaf@users.noreply.github.com>
2022-09-26 11:06:44 +0200

    Add files
    Added previously made files for the project

---
+++ js/main.js
@@ -0,0 +1,51 @@
+const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+const ApiUse = BASE_URL + '/movie/upcoming?' +API_KEY;
+var visible = true;
+
+var settings = {
+    "url": "https://api-gate2.movieglu.com/filmsNowShowing/?n=15",
+    "method": "GET",
+    "timeout": 0,
+    "headers": {
+    "api-version": "v200",
+    "Authorization": "Basic Q1JUTV9YWDpsTUhGWGU3azJ6QVk=",
+    "client": "CRTM",
+    "x-api-key": "N1VKriN0z01rTs3lakrgn1zGCxtjBZa03ryXBwR3",
+    "device-datetime": "2022-09-22T10:07:57.296Z",
+    "territory": "DE",
+    },
+    };
+    $.ajax(settings).done(function (response) {
+        var num = 0;
+        console.log(response)
+        response.films.forEach(i => {
+            if(i.film_trailer === null){
+                var onclick = `<p>No trailer</p><br>`
+            }
+            else{
+                var onclick = `<a href="`+i.film_trailer+`">View Trailer</a><br>`
+            }
+            if(i.images.poster[1] === undefined){
+                var img = `<h2>No img found</h2>`
+            }
+            else{
+                var img = `<img src=${i.images.poster[1].medium.film_image} alt='movie poster'><br>`
+            }
+            var title = `<h5 class="card-title">${i.film_name}</h5><br>`
+            var rating = `<p class="card-text">${i.age_rating[0].rating}</p><br>`
+            var description = `<p class="card-text">${i.synopsis_long}</p><br>`
+            var movie = (`<div class="mainDiv card-mainVC"><div class="card-body id="card`+num+`">${img+title}<div class="content Collapsed">${onclick+rating+description}</div></div></div>`)
+            $(".cards").append(movie);    
+            num++;                
+        });
+    });
+
+let toggleCol = () => {
+    $(".content").toggleClass("Collapsed");
+    $(".mainDiv").toggleClass(" card-mainVC card-mainVNC");
+    $("#cardsMain").toggleClass("cards cardsTog")
+}
