f32fa6c463d74d7bb02aad1f314e1457d01690ae
MfeMeaf <80445981+MfeMeaf@users.noreply.github.com>
2022-09-27 14:00:44 +0200

    +trailer website
    Added trailer website + minor fixes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 var settings = {
-    "url": "https://api-gate2.movieglu.com/filmsNowShowing/?n=13",
+    "url": "https://api-gate2.movieglu.com/filmsNowShowing/?n=15",
     "method": "GET",
     "timeout": 0,
     "headers": {
     "api-version": "v200",
     "Authorization": "Basic Q1JUTV9YWDpsTUhGWGU3azJ6QVk=",
     "client": "CRTM",
-    "x-api-key": "qsuWSsxtQD3NxklgDcElz5MPEQVCpxCq406P3YY7",
+    "x-api-key": "N1VKriN0z01rTs3lakrgn1zGCxtjBZa03ryXBwR3",
     "device-datetime": "2022-09-27T10:07:57.296Z",
-    "territory": "DE",
+    "territory": "XX",
     },
     };
     $.ajax(settings).done(function (response) {
@@ -19,7 +19,7 @@ var settings = {
                 var onclick = `<p>No trailer</p><br>`
             }
             else{
-                var onclick = `<a href="`+i.film_trailer+`">View Trailer</a><br>`
+                var onclick = `<a href="trailer.html?imdb_id=${i.imdb_id}">View Trailer</a><br>`
             }
             if(i.images.poster[1] === undefined){
                 var img = `<h2>No img found</h2>`
@@ -29,15 +29,8 @@ var settings = {
             }
             var title = `<h5 class="card-title">${i.film_name}</h5><br>`
             var rating = `<p class="card-text">${i.age_rating[0].rating}</p><br>`
-            var description = `<p class="card-text">${i.synopsis_long}</p><br>`
-            var movie = (`<div class="mainDiv card-mainVC"><div class="card-body id="card`+num+`">${img+title}<div class="content Collapsed">${onclick+rating+description}</div></div></div>`)
+            var movie = (`<div class="mainDiv card-mainVC"><div class="card-body id="card`+num+`">${img+title+onclick}<div>${rating}</div></div></div>`)
             $(".cards").append(movie);    
             num++;                
         });
     });
-
-let toggleCol = () => {
-    $(".content").toggleClass("Collapsed");
-    $(".mainDiv").toggleClass(" card-mainVC card-mainVNC");
-    $("#cardsMain").toggleClass("cards cardsTog")
-}
---
+++ js/trailer.js
@@ -0,0 +1,34 @@
+const url = new URL(window.location.href)
+console.log(url)
+console.log(url.search)
+const id = url.search.slice(9)
+var settings = {
+    "url": "https://api-gate2.movieglu.com/filmsNowShowing/?n=15",
+    "method": "GET",
+    "timeout": 0,
+    "headers": {
+    "api-version": "v200",
+    "Authorization": "Basic Q1JUTV9YWDpsTUhGWGU3azJ6QVk=",
+    "client": "CRTM",
+    "x-api-key": "N1VKriN0z01rTs3lakrgn1zGCxtjBZa03ryXBwR3",
+    "device-datetime": "2022-09-27T10:07:57.296Z",
+    "territory": "XX",
+    },
+    };
+$.ajax(settings).done(function (response) {
+    console.log(response.films)
+    response.films.forEach(i => {
+        if(i.imdb_id == id){
+            var title = `<h2 class="card-title">${i.film_name}</h2><br>`
+            var Synopsis = `<h3> Synopsis of ${i.film_name}</h3><br> `
+            var description = `<p class="card-text">${i.synopsis_long}</p><br>`
+            var trailer = `<video controls autoplay><source src=${i.film_trailer} type="video/mp4">Your browser does not support the video tag.</video>`
+            var movie = `<div class="movieTrailer"><div class="trailer-body">${title+trailer}</div></div>`
+            var movieDesc = `<div class="movieDesc"><div class="trailer-body">${Synopsis + description}</div></div>`
+            $(".trailer").append(movie);
+            $(".description").append(movieDesc)
+        }
+        else
+        console.log("false")
+    });
+});
